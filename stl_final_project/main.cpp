4e1b5e4141c7f640cffd1edc2a31723d797fcf95
Mateusz Muszak <mateusz.muszak.ext@nokia.com>
2016-11-18 14:31:24 +0100

    Initial commit for stl project

---
+++ stl_final_project/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+std::vector<int> numVector;
+
+int main()
+{
+    for(int i = 1; i <1001; ++i)
+    {
+        numVector.push_back(i);
+    }
+    auto vec_it = numVector.begin();
+
+    return 0;
+}
